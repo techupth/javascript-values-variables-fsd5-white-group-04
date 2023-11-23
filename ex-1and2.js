b2c5feb3f3f3d2d0c1fbd00f20ebd786d6287e66
Yupaporn R. <lyupaporn.r@gmail.com>
2023-11-23 11:05:11 +0700

    feat(values-variables): add completed exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let restaurantAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const billTotalPrice = 15000;
+const numberOfOpeningRestaurants = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+restaurantAddress =
+  "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+billTotalPrice = 5000;
+numberOfOpeningRestaurants = 2500;
